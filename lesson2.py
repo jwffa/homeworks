167229fcd867bc985947fa50a3eea9f395e40724
jwffa <146485313+jwffa@users.noreply.github.com>
2023-09-30 16:25:34 +0400

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,4 @@
+username = input('What is your name? ')
+age = input('How old are you?')
+fav_color = input('What is your favorite color? ')
+print(username + ' is ' + age + ' years old and he likes ' + fav_color)
