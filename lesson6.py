aa5062b34d23ba9f5cc4a0814f38a3d66820a92a
jwffa <146485313+jwffa@users.noreply.github.com>
2023-10-08 18:26:57 +0400

    Add files via upload

---
+++ lesson6.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age: "))
+father_age = int(input("Enter your father's name: "))
+year = 2023
+for i in range(5):
+    print(" შენ და მამაშენი " + str(year + i) + " წელს მამაშენი იქნება " + str(father_age + i) + " წლის და შენ იქნები " + str(age + i) + 
+           " წლის,  ხოლო მამაშენი შენზე უფროსი იქნება " + str(father_age - age) + " წლით ")
