244d836b42959ca2a120560281bdf10da56ea544
jwffa <146485313+jwffa@users.noreply.github.com>
2023-10-12 02:49:58 +0400

    Add files via upload

---
+++ lesson8.py
@@ -0,0 +1,9 @@
+salary = int(input("How much is your salary?"))
+if salary > 10000:
+    print(" Did you study at Goa?")
+elif salary > 1000:
+    if salary < 10000:
+        print(" You mid...")
+else:
+    salary < 1000
+    print(" You should have studied at Goa!!! ") 
