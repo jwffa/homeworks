669d200d296834b08f2b013fb71709c3107db9a4
jwffa <146485313+jwffa@users.noreply.github.com>
2023-10-12 22:09:50 +0400

    Update lesson8.py

--- lesson8.py
+++ lesson8.py
@@ -1,9 +1,8 @@
 salary = int(input("How much is your salary?"))
 if salary > 10000:
     print(" Did you study at Goa?")
-elif salary > 1000:
-    if salary < 10000:
-        print(" You mid...")
+elif salary > 1000 and salary < 10000:
+     print(" You mid...")
 else:
     salary < 1000
     print(" You should have studied at Goa!!! ") 
