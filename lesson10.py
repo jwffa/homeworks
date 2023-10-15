c0513f9bea6799248ba4db15858b67ec2ca93aed
jwffa <146485313+jwffa@users.noreply.github.com>
2023-10-16 01:58:56 +0400

    Add files via upload

---
+++ lesson10.py
@@ -0,0 +1,4 @@
+members = ["Lionel Messi", "Lela Tsurtsumia", "Gia Suramelashvili",
+           "Adriana Lima", "Ertaoz Bregvadze", "Gocha Chetkoevi",
+           "Diana 'The Eagle' Ghurtskaia", "Ioseb Jughashvili"]
+print(members[0:7])
