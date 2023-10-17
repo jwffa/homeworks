d000f9cdfe1fb4f49467c38b3fff19df78d0b92f
jwffa <146485313+jwffa@users.noreply.github.com>
2023-10-18 03:22:22 +0400

    Add files via upload

---
+++ lesson16.py
@@ -0,0 +1,10 @@
+#HARDEST BOSS HW - VERSION 1
+
+#barobaro = ['Hello', 'World', 'King', 'Is', 'Here']
+#print(barobaro[0:2])
+
+#HARDEST BOSS HW - VERSION 2
+
+otxi_gia = ['Hello', 'World']
+m, a = otxi_gia
+print(m, a)
