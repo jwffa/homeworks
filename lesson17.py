7769c8c0d130a5ff873b9701325335f55163b696
jwffa <146485313+jwffa@users.noreply.github.com>
2023-10-22 00:05:07 +0400

    Add files via upload

---
+++ lesson17.py
@@ -0,0 +1,21 @@
+#ცოტნე სართანიას ამოცანა
+
+exmp = int(input("67-23 ="))
+exmp1 = int(input("30+10 ="))
+exmp2 = int(input("67+23 ="))
+exmp3 = int(input("31+20 ="))
+exmp4 = int(input("10+9 ="))
+exmp5 = int(input("10+18 ="))
+exmp6 = int(input("5+17 ="))
+exmp7 = int(input("25+23 ="))
+exmp8 = int(input("10+10 ="))
+exmp9 = int(input("70+10 ="))
+
+if exmp == 44:
+    print(exmp + " you have 1 point")
+
+
+   
+    
+
+
