fc32b5b218957569041a4c362cf7f7aaef9d4eb1
jwffa <146485313+jwffa@users.noreply.github.com>
2023-10-22 00:18:55 +0400

    Update lesson17.py

--- lesson17.py
+++ lesson17.py
@@ -1,19 +1,43 @@
 #ცოტნე სართანიას ამოცანა
 
 exmp = int(input("67-23 ="))
+if exmp == 44:
+    print("Nice! You have 1 point.")
 exmp1 = int(input("30+10 ="))
+if exmp1 == 40:
+    print("Nice! You have 2 points.")
 exmp2 = int(input("67+23 ="))
+if exmp2 == 90:
+    print("Nice! You have 3 points.")
 exmp3 = int(input("31+20 ="))
+if exmp3 == 51:
+    print("Nice! You have 4 points.")
 exmp4 = int(input("10+9 ="))
+if exmp4 == 19:
+    print("Wonderful! You have 5 points.")
 exmp5 = int(input("10+18 ="))
+if exmp5 == 28:
+    print("Amazing! You have 6 points.")
 exmp6 = int(input("5+17 ="))
+if exmp6 == 22:
+    print("Saghol bidzia shen! You have 7 points.")
 exmp7 = int(input("25+23 ="))
+if exmp7 == 48:
+    print("WOOOOOOW! You have 8 points.")
 exmp8 = int(input("10+10 ="))
+if exmp8 == 20:
+    print("Ancara messi, ancara messi!!! You have 9 points.")
 exmp9 = int(input("70+10 ="))
+if exmp9 == 80:
+    print("GOAT LIKE GOA!!! You have 10 points.")
 
 if exmp == 44:
-    print(exmp + " you have 1 point")
+    print("Nice! You have 1 point.")
 
+if exmp == 44 and exmp1 == 40 and exmp2 == 90 and exmp3 == 51 and exmp4 == 19 and exmp5 == 28 and exmp6 == 22 and exmp7 == 48 and exmp8 == 20 and exmp9 == 80:
+    print("ყოჩაღ! ყველა პასუხი სწორია. <3")
+else:
+    print("მოდი გოაში!")
 
    
     
