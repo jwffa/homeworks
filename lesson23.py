de6f498bd090097bcfcdae607cb66599f0674565
jwffa <146485313+jwffa@users.noreply.github.com>
2023-10-23 05:47:01 +0400

    Add files via upload

---
+++ lesson23.py
@@ -0,0 +1,12 @@
+#Salome Miladze-ს ამოცანა
+
+myfilm = ["Pulp Fiction", "12 Agnry men", "The Shawshank Redemption",
+          "The Matrix", "City of God", "The Prestige", "Siberian Education"]
+
+friendfilm = ["Parasite", "It's a Wonderful Life", "The Matrix"]
+
+for friendfilm in myfilm:
+    if friendfilm == "The Matrix":
+        print("ეგ გავლილი მაქვს, ვიცი")
+        if friendfilm != "The Matrix":
+            print("ეგ ფილმი არ მაქვს ნანახი")
