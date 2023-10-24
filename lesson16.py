9d7dc371680566cffcb42773c203d2f7a58601d5
jwffa <146485313+jwffa@users.noreply.github.com>
2023-10-25 02:10:53 +0400

    Update lesson16.py

--- lesson16.py
+++ lesson16.py
@@ -1,9 +1,6 @@
-#HARDEST BOSS HW - VERSION 1
+#HARDEST BOSS HW
 
-#barobaro = ['Hello', 'World', 'King', 'Is', 'Here']
-#print(barobaro[0:2])
 
-#HARDEST BOSS HW - VERSION 2
 
 otxi_gia = ['Hello', 'World']
 m, a = otxi_gia
