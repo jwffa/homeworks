14f5ad02e2e1f83b1b7967c4aabac892853559c4
jwffa <146485313+jwffa@users.noreply.github.com>
2023-11-29 01:11:38 +0400

    Add files via upload

---
+++ codewars7.py
@@ -0,0 +1,30 @@
+def example_sort(arr, example_arr):
+    my_arr = []
+    for i in example_arr:
+        if i in arr:
+            my_arr.append(i)
+    return my_arr
+print(example_sort([1,2,3,4,5],[2,3,4,1,5])) 
+
+
+    
+
+
+
+
+
+
+
+
+
+
+        
+
+        
+
+
+
+
+
+
+            
