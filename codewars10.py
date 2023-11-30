b03b8458f9f183b854761c2965e0a0486c6862b4
jwffa <146485313+jwffa@users.noreply.github.com>
2023-11-30 05:56:16 +0400

    Update codewars10.py

--- codewars10.py
+++ codewars10.py
@@ -1,4 +1,4 @@
-def domain_name(url):
+def domain_name(url): #5kyu
     replaced_url = url.replace("http://", "").replace("https://", "").replace("www.", "").replace(".com", "").replace("/kata/", "")
     replaced_url = replaced_url.split(".")
     return replaced_url[0]
